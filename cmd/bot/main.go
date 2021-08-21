b00ce924b20310f7b72e11c757fcd8581e1f0085
Vishhvaan Gopalakrishnan <vish0288@gmail.com>
2021-08-21 09:25:22 -0400

    initial commit

---
+++ cmd/bot/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/socketmode"
+	"github.com/slack-go/slack/slackevents"
+)
+
+func main() {
+
+}
