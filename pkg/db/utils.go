3138bd8ce6dc10b53634b9585c48a8dfeef2382c
vishhvaan <vish0288@gmail.com>
2022-09-01 15:58:08 -0400

    read and update strings in database

--- pkg/db/utils.go
+++ pkg/db/utils.go
@@ -22,9 +22,43 @@ func OpenDB() {
 	dbPath := path.Join(exePath, dbFile)
 
 	var err error
-	botDB.db, err = bolt.Open(dbPath, 0644, nil)
+	botDB.db, err = bolt.Open(dbPath, 0600, nil)
 	if err != nil {
 		botDB.logger.WithError(err).Panic("database could not be opened")
 	}
 	defer botDB.db.Close()
+}
+
+func AddStringValue(bucket string, key string, value string) {
+	err := botDB.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		err := b.Put([]byte(key), []byte(value))
+		return err
+	})
+
+	if err != nil {
+		botDB.logger.WithError(err).WithFields(log.Fields{
+			"bucket": bucket,
+			"key":    key,
+			"value":  value,
+		}).Error("Cannot update database")
+	}
+}
+
+func ReadStringValue(bucket string, key string) (value string) {
+	err := botDB.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		bytes := b.Get([]byte(key))
+		value = string(bytes)
+		return nil
+	})
+
+	if err != nil {
+		botDB.logger.WithError(err).WithFields(log.Fields{
+			"bucket": bucket,
+			"key":    key,
+		}).Error("Cannot read database")
+	}
+
+	return value
 }
