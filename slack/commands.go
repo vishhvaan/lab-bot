6ab3a5c841f1c799840041e6efbbd527297bada7
vishhvaan <vish0288@gmail.com>
2025-06-22 21:51:38 -0400

    fix finding usernames

--- slack/commands.go
+++ slack/commands.go
@@ -24,7 +24,18 @@ func (sc *slackClient) getChannelName(channelID string) (channel string) {
 func (sc *slackClient) getUserName(userID string) (user string) {
 	us, err := sc.api.GetUserInfo(userID)
 	if err != nil {
-		sc.logger.WithField("err", err).Error("Couldn't find conversation info.")
+		sc.logger.WithField("userID", userID).WithField("err", err).
+			Warn("couldn't fetch user info; falling back to mention")
+		return "<@" + userID + ">"
 	}
-	return us.Profile.DisplayName
+
+	name := us.Profile.DisplayName
+	if name == "" {
+		name = us.Profile.RealName
+	}
+	if name == "" {
+		name = "<@" + userID + ">"
+	}
+
+	return name
 }
